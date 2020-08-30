e1eef27ac65c8d9886dfbc90003d9d661fdc06d4
lindh04 <47460872+lindh04@users.noreply.github.com>
2020-08-30 21:39:51 +0200

    express keep alive

--- src/commands/moderation/PurgeCommand.ts
+++ src/commands/moderation/PurgeCommand.ts
@@ -1,6 +1,20 @@
 import { Message, Base } from "discord.js";
 import BaseCommand from "../../utils/structures/BaseCommand";
 import DiscordClient from "../../client/client";
+import express = require("express");
+import http = require("http");
+const app = express();
+
+if (process.env.PROJECT_DOMAIN) {
+  app.get("/", (request, response) => {
+    console.log(Date.now() + " Ping Received");
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT);
+  setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  }, 280000);
+}
 
 export default class PurgeCommand extends BaseCommand {
   constructor() {
@@ -17,6 +31,7 @@ export default class PurgeCommand extends BaseCommand {
     const fetched = await message.channel.messages.fetch({
       limit: deleteCount + 1,
     });
+
     await fetched.forEach((msg) => {
       msg.delete().catch((error) => message.reply(`Error: ${error}`));
     });
