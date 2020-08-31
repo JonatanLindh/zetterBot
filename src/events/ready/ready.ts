673dcdede3da80633a1588b8e72e696d3b2cc8b8
lindh04 <47460872+lindh04@users.noreply.github.com>
2020-08-31 21:05:41 +0200

    status

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -1,11 +1,18 @@
-import BaseEvent from '../../utils/structures/BaseEvent';
-import DiscordClient from '../../client/client';
+import BaseEvent from "../../utils/structures/BaseEvent";
+import DiscordClient from "../../client/client";
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
-    super('ready');
+    super("ready");
   }
-  async run (client: DiscordClient) {
-    console.log('Bot has logged in.');
+  async run(client: DiscordClient) {
+    await client.user.setStatus("online");
+    await client.user.setPresence({
+      activity: {
+        name: "kids screaming",
+        type: "LISTENING",
+      },
+    });
+    console.log("Bot has logged in.");
   }
 }
